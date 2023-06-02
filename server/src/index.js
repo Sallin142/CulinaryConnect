63895290ab9fdd26b62346e5cf97e14d2f2b149a
ska287 <ska287@sfu.ca>
2023-06-02 14:37:02 -0700

    add catch all method to express

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,24 @@ import express from "express"
 import cors from  "cors"
 import mongoose from "mongoose"
 import {recipeRouter} from "./routes/recipes.js";
-
+import path from "path";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/recipes", recipeRouter);
 
-//create enviroment variable
+//create environment variable
 mongoose.connect("mongodb+srv://sallinKoutev:Nillas2468@recipe-app.cff9zjm.mongodb.net/Recipe-app?retryWrites=true&w=majority")
+
+// Serve static files from the React app
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '372-asn-1/client/build')));
+
+// The catchall handler: for any request that doesn't match one above, 
+// send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/372-asn-1/client/build/index.html'));
+});
 
 app.listen(3001, () => console.log("server start"));
