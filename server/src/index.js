a79642dd9e8ecfc3a405b714ab9638a55ba65e8c
Sallin <Sallin.koutev@gmail.com>
2023-08-20 00:47:43 -0700

    init

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,23 @@
-import express from "express"
-import cors from  "cors"
-import mongoose from "mongoose"
-import {recipeRouter} from "./routes/recipes.js";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { recipeRouter } from "./routes/recipes.js";
+import { ingredientRouter } from "./routes/ingredients.js";
 import path from "path";
+import session from "express-session";
+import axios from "axios";
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(session({ secret: "your-secret-key", resave: true, saveUninitialized: true }));
+dotenv.config();
+// Make sure CORS is set up before your routes
 app.use("/recipes", recipeRouter);
+app.use("/ingredients", ingredientRouter);
+// Create environment variable
+mongoose.connect(process.env.MONGODB_URL);
 
-//create environment variable
-mongoose.connect("mongodb+srv://sallinKoutev:Nillas2468@recipe-app.cff9zjm.mongodb.net/Recipe-app?retryWrites=true&w=majority")
-
-// Serve static files from the React app
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '372-asn-1/client/build')));
-
-// The catchall handler: for any request that doesn't match one above, 
-// send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/372-asn-1/client/build/index.html'));
-});
-
-app.listen(3001, () => console.log("server start"));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log("Server started on port", port));
--- server/src/models/Recipe.js
+++ server/src/models/Recipe.js
@@ -3,24 +3,35 @@ import mongoose from "mongoose";
 const RecipeSchema = mongoose.Schema({
   name: {
     type: String,
-    required: false,
+    required: true,
   },
   ingredients: [
     {
-      type: String,
-      required: false,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ingredient',
+      required: true,
     },
   ],
   instructions: {
     type: String,
-    required: false,
+    required: true,
   },
-
   imageUrl: {
     type: String,
     required: false,
   },
+  lastModified: { 
+    type: Date, 
+    default: Date.now 
+  },
+}, {
+  timestamps: true
+});
 
+// Middleware to set lastModified to the current date on each save
+RecipeSchema.pre('save', function (next) {
+  this.lastModified = Date.now();
+  next();
 });
 
 export const RecipesModel = mongoose.model("Recipes", RecipeSchema);
